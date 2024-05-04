dc93e33915e86924e792f16e4efe7791aaf1e29a
ninanee <ni.yun@outlook.com>
2024-05-04 17:54:59 -0400

    finished first task

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var progressView: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [
+        "2 + 3 == 5?",
+        "5 - 3 == 2?",
+        "3 - 8 >= 10?",
+    ]
+    
+    var questionNum = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        updateUI()
+            }
+
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        questionNum += 1
+        updateUI()
+
     }
-
-
+    
+    func updateUI() {
+        
+        progressView.text = quiz[questionNum]
+    }
 }
 
